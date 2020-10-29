e8da2f007cf60d6f097b61414a4a0311d98d9262
Haonan Wang <72527916+hwang164@users.noreply.github.com>
2020-10-28 23:54:52 -0400

    Add files via upload

---
+++ sentimentanalysis.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def sentimentanalysis():
+    from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+    # Call vader sentiment
+    vad=SentimentIntensityAnalyzer()
+    # Created a new column to record sentiment analysis result
+    file["score"]=file["purpose"].apply(lambda x:vad.polarity_scores(x)['compound'])
+    # Find the company with the max and min score
+    max=file.iloc[file["score"].idxmax()]["name"]
+    min=file.iloc[file["score"].idxmin()]["name"]
+    print("Best business idea:", max)
+    print("Worst business idea:", min)
+    
+
+if __name__ == "__main__":
+    sentimentanalysis()
+
