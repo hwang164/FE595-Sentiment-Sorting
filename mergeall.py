882ab759b734080d16dec1d2a8e182f6124156f4
Haonan Wang <72527916+hwang164@users.noreply.github.com>
2020-10-28 23:30:28 -0400

    Add files via upload

---
+++ mergeall.py
@@ -0,0 +1,26 @@
+def mergeall():
+    file1=pd.read_csv('C:/Users/lenovo/Documents/FE595-HW3/result.txt', sep=("---"), header=None)
+    file1[0] = file1[0].str[6:]
+    file1[1] = file1[1].str[9:]
+    file1.rename(columns={0:'name',1:'purpose'}, inplace = True)
+    
+    file2=pd.read_csv('C:/Users/lenovo/Documents/FE595-HW3/result.csv')
+    
+    file3=pd.read_csv('C:/Users/lenovo/Documents/FE595-HW3/company_list.csv')
+    
+    file4=pd.read_csv('C:/Users/lenovo/Documents/FE595-HW3/Mydata.csv')
+    del file4['Unnamed: 0']
+    file4.rename(columns={'Company name':'name','Company purpose':'purpose'}, inplace = True)
+    
+    file5=pd.read_csv('C:/Users/lenovo/Documents/FE595-HW3/FE595Data1.csv')
+    
+    file=pd.concat([file1,file2])
+    file=pd.concat([file,file3])
+    file=pd.concat([file,file4])
+    file=pd.concat([file,file5])
+    
+    file=file.reset_index(drop=True)
+
+
+if __name__ == "__main__":
+    mergeall()
