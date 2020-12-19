7c53dab8c66345f5b5d7ea07dd17ee38c8eb89d8
Haonan Wang <72527916+hwang164@users.noreply.github.com>
2020-12-19 02:42:40 +0000

    Update mergeall.py

--- mergeall.py
+++ mergeall.py
@@ -1,5 +1,7 @@
+import csv
+import pandas as pd
+    
 def mergeall():
-    import pandas as pd
     # Change the data in .txt file to the same format with other .csv files
     file1=pd.read_csv('C:/Users/lenovo/Documents/FE595-HW3/result.txt', sep=("---"), header=None)
     file1[0] = file1[0].str[6:]
@@ -22,6 +24,8 @@ def mergeall():
     file=pd.concat([file,file5])
     
     file=file.reset_index(drop=True)
+    
+    file.to_csv('C:/Users/lenovo/Documents/FE595-HW3/all.csv', index=False, encoding='utf-8-sig')
 
 
 if __name__ == "__main__":
