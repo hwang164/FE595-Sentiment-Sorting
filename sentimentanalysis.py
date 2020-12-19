a455e33b3547c0b7c9411f4cd7c1a7ae1138e07a
Haonan Wang <72527916+hwang164@users.noreply.github.com>
2020-12-19 02:35:46 +0000

    Update sentimentanalysis.py

--- sentimentanalysis.py
+++ sentimentanalysis.py
@@ -1,15 +1,20 @@
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import pandas as pd
+    
 def sentimentanalysis():
-    from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+    file=pd.read_csv('C:/Users/lenovo/Documents/FE595-HW3/all.csv')
+    
     # Call vader sentiment
     vad=SentimentIntensityAnalyzer()
+    
     # Created a new column to record sentiment analysis result
     file["score"]=file["purpose"].apply(lambda x:vad.polarity_scores(x)['compound'])
+    
     # Find the company with the max and min score
     max=file.iloc[file["score"].idxmax()]["name"]
     min=file.iloc[file["score"].idxmin()]["name"]
-    print("Best business idea:", max)
-    print("Worst business idea:", min)
-    
+    print("Best business idea:",max)
+    print("Worst business idea:",min)
 
 if __name__ == "__main__":
     sentimentanalysis()
